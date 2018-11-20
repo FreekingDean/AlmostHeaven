f2e9624161a67309eb74731e579bd259c28b749b
Dean <deangalvin3@gmail.com>
2018-11-20 09:51:29 -0500

    Added needed imports

--- log.go
+++ log.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func Log(w http.ResponseWriter, _ *http.Request) {
 	resp := buildPlatformSuccess(nil)
-	err = json.NewEncoder(w).Encode(resp)
+	err := json.NewEncoder(w).Encode(resp)
 	if err != nil {
 		log.Println(err)
 	}
