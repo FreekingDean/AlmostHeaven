f27ac0d9768d35fbb1a680d827b183cc47038023
Dean <deangalvin3@gmail.com>
2018-11-20 17:12:44 -0500

    Actually log stuff

--- log.go
+++ log.go
@@ -6,9 +6,16 @@ import (
 	"net/http"
 )
 
-func Log(w http.ResponseWriter, _ *http.Request) {
+type LogRequest struct {
+	Error string `json:"error"`
+}
+
+func Log(w http.ResponseWriter, r *http.Request) {
+	l := &LogRequest{}
+	err := json.NewDecoder(r.Body).Decode(&l)
+	log.Println(l.Error)
 	resp := buildPlatformSuccess(nil)
-	err := json.NewEncoder(w).Encode(resp)
+	err = json.NewEncoder(w).Encode(resp)
 	if err != nil {
 		log.Println(err)
 	}
